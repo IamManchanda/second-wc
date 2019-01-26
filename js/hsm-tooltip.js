670b8a97d40627c992fd023dc88550f40ce6e7c9
Harry Manchanda <harmanmanchanda182@gmail.com>
2019-01-26 21:04:17 +0530

    More styling updates!

--- js/hsm-tooltip.js
+++ js/hsm-tooltip.js
@@ -13,14 +13,17 @@ class HSMTooltip extends HTMLElement {
           border-radius: .5rem;
           margin-top: .5rem;
           position: absolute;
+          box-shadow: 0 2px 5px 0 rgba(0, 0, 0, 0.16), 0 2px 10px 0 rgba(0, 0, 0, 0.12);
           z-index: 10;
+          font-size: 1.05rem;
         }
-        span.highlight {
+        span.icon {
           background-color: #0a0a0a;
           color: #fefefe;
           padding: 0.15rem 0.5rem;
           text-align: center;
           border-radius: 50%;
+          box-shadow: 0 2px 5px 0 rgba(0, 0, 0, 0.16), 0 2px 10px 0 rgba(0, 0, 0, 0.12);
           cursor: help;
         }
         ::slotted(span.highlight) {
@@ -28,18 +31,24 @@ class HSMTooltip extends HTMLElement {
         }
       </style>
       <slot>Components</slot>
-      <span class="highlight">?</span>
+      <span class="icon">?</span>
     `;
   }
 
   connectedCallback() {
-    if (this.hasAttribute('hsm-text')) {
-      this._tooltipText = this.getAttribute('hsm-text');
-    }
-    const tooltipIcon = this.shadowRoot.querySelector('span');
+    if (this.hasAttribute('hsm-text')) this._tooltipText = this.getAttribute('hsm-text');
+    const tooltipIcon = this.shadowRoot.querySelector('span.icon');
     tooltipIcon.addEventListener('mouseenter', this._showTooltip.bind(this));
     tooltipIcon.addEventListener('mouseleave', this._hideTooltip.bind(this));
-    this.shadowRoot.appendChild(tooltipIcon);
+  }
+
+  attributeChangedCallback(name, oldValue, newValue) {
+    if (oldValue === newValue) return;
+    if (name === 'hsm-text') this._tooltipText = newValue;
+  }
+
+  static get observedAttributes() {
+    return ['hsm-text'];
   }
 
   _showTooltip() {
